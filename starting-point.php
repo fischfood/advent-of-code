6cd63183a6f245d565d4f19fe3c01ab0a6b7a984
Brian Fischer <contact@brianfischer.me>
2023-09-12 02:34:16 -0400

    AOC: Renaming 2022, adding bash scripts to generate AoC files and folders more easily through the command line

---
+++ starting-point.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Day DAY: TITLE
+ */
+
+// The usual
+$data = file_get_contents('data/data-DAY.txt');
+//$data = file_get_contents('data/data-DAY-sample.txt');
+
+$rows = explode("\n", $data);
+
+
+// Part One
+function part_one() {
+    # Do Stuff
+}
+
+// Part Two
+function part_two() {
+	# Do More Things
+}
+
+part_one();
+part_two();
