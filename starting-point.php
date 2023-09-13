ed08d636b66c7a262e61ba9af7ea87fe59c383c5
Brian Fischer <brian@linchpin.com>
2023-09-13 01:42:31 -0400

    AOC: Dataset setup and output strings

--- starting-point.php
+++ starting-point.php
@@ -10,16 +10,18 @@ $data = file_get_contents('data/data-DAY.txt');
 
 $rows = explode("\n", $data);
 
+$dataset = $rows;
 
 // Part One
-function part_one() {
+function part_one($dataset) {
     # Do Stuff
 }
 
 // Part Two
-function part_two() {
+function part_two($dataset) {
 	# Do More Things
 }
 
-part_one();
-part_two();
+echo PHP_EOL . 'Day DAY: TITLE' . PHP_EOL;
+echo 'Part 1: ' . part_one($dataset) . PHP_EOL;
+echo 'Part 2: ' . part_two($dataset) . PHP_EOL;
