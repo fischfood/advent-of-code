1921cf00f08c22aa98e564a57dd0e9b59874e382
Brian Fischer <contact@brianfischer.me>
2023-12-01 09:52:31 -0500

    AOC(2023): Day 1 - Adding notes

--- 2023/01.php
+++ 2023/01.php
@@ -17,10 +17,14 @@ $dataset = $rows;
 function part_one($dataset) {
     $total = 0;
     foreach( $dataset as $d ) {
+
+        // Only give me digits
         $nums = str_split( preg_replace( '/[^0-9]/', '', $d ) );
 
         // Combine first and last numbers, even if there is only one
         $number = intval( $nums[0] . end($nums) );
+
+        // Add to the Total
         $total += $number;
     }
 
@@ -33,18 +37,22 @@ function part_two($dataset) {
     foreach( $dataset as $d ) {
 
         $num_strings = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        // $num_digits = [1,2,3,4,5,6,7,8,9];
-        // I Initially read it as eightwo would only be 8, so I tried to replace in order. This was wrong, it should be 8 and 2
 
         // Convert the text to a digit, but leave the rest of the number in case characters overlap
+        // Ex. eightwo should become 82, so when we convert 2, we need a t and the front, and 0 at the end.
+        // I just added the whole string for clarity instead of o1e, t2o, t3e, etc.
         $num_d_s = ['one1one', 'two2two', 'three3three', 'four4four', 'five5five', 'six6six', 'seven7seven', 'eight8eight', 'nine9nine'];
 
+        // String to digit string conversions
         $d2 = str_replace( $num_strings, $num_d_s, $d );
+
+        // Only give me digits
         $nums = str_split( preg_replace( '/[^1-9]/', '', $d2 ) );
 
+        // Combine first and last numbers, even if there is only one
         $number = intval( $nums[0] . end($nums) );
 
-        //echo PHP_EOL . $number . ' - - - ' . $d . '(' . implode('',$nums) . ') = (' . $nums[0] . ' + ' . end($nums) . ')';
+        // Add to the total
         $total += $number;
     }
 
