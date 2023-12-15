ba24808e800d52d0da9314b218e822c4845461b3
Brian Fischer <brian@linchpin.com>
2023-12-15 00:50:18 -0500

    AOC(2023): Day 15 - Part Two Complete

--- 2023/15.php
+++ 2023/15.php
@@ -35,7 +35,44 @@ function part_one($strings) {
 
 // Part Two
 function part_two($strings) {
-	# Do More Things
+	
+    $contents = array_fill(0,256, [] );
+
+    foreach( $strings as $row => $string ) {
+
+        $label = preg_replace( '/[^a-z]/', '', $string );
+        $lens = preg_replace( '/[^0-9]/', '', $string );
+
+        $characters = str_split( $label );
+
+        $total = 0;
+        foreach( $characters as $char ) {
+            
+            $total += ord($char);
+            $total = $total * 17;
+            $total = $total % 256;
+        }
+
+        $box = $total;
+
+        // If remove
+        if ( '' == $lens ) {
+            unset( $contents[$box][$label] );
+        } else {
+            $contents[$box][$label] = $lens;
+        }
+    }
+
+    $focus_power = 0;
+
+    foreach( array_filter( $contents ) as $box_level => $box_contents ) {
+        $box_contents = array_values( $box_contents );
+        foreach( $box_contents as $slot => $lenses ) {
+            $focus_power += ($box_level + 1) * ($slot + 1) * preg_replace( '/[^0-9]/', '', $lenses );
+        }
+    }
+
+    echo $focus_power;
 }
 
 echo PHP_EOL . 'Day 15: Lens Library' . PHP_EOL . 'Part 1: ';
