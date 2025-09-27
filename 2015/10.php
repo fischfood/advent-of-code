086717a6a9d3071e13169e328c4f1bd4332b2dd3
Brian Fischer <brian@linchpin.com>
2025-09-27 00:29:30 -0400

    AOC(2015): Day 10 Complete

--- 2015/10.php
+++ 2015/10.php
@@ -3,47 +3,53 @@ ini_set('memory_limit', '10G');
 
 /**
  * Day 10: TITLE
+ * Part 1: .15752 Seconds
+ * Part 2: 2.7098 Seconds
  */
 
 // The usual
 $data = 1321131112;
 //$data = 1;
 
+$starttime = microtime(true);
+
 // Part One
 function look_and_say( $data, $limit ) {
 
-    $ds = $data;
+	$ds = $data;
 
-    for ( $i = 1; $i <= $limit; $i++ ) {
-        $j = 0;
-        $current = '';
+	for ( $i = 1; $i <= $limit; $i++ ) {
 
-        //echo 'checking ' . $ds . "\n";
-        $d = str_split( $ds );
-        $ds = '';
-        $da = [];
+		$d = str_split( $ds );
+		$ds = '';
 
-        foreach( $d as $num ) {
-            if ( $num === $current ) {
-                $total = $da[$j][1];
-                $da[$j] = [$num, $total + 1];
-            } else {
-                $j++;
-                $da[$j] = [$num, 1];
-            }
-            $current = $num;
-        }
+		$current = $d[0];
+		$count   = 1;
+		$output  = [];
 
-        foreach( $da as $k => $da_data ) {
-            $ds = $ds . $da_data[1] . $da_data[0];
-        }
-    }
+		for ( $j = 1, $len = count( $d ); $j < $len; $j++ ) {
+			if ( $d[$j] === $current ) {
+				$count++;
+			} else {
+				$output[] = $count;
+				$output[] = $current;
+				$current  = $d[$j];
+				$count    = 1;
+			}
+		}
 
-    echo strlen( $ds );
+		$output[] = $count;
+		$output[] = $current;
+
+		$ds = implode( '', $output );
+	}
+
+	echo strlen( $ds );
 }
 
 echo PHP_EOL . 'Day 10: TITLE' . PHP_EOL . 'Part 1: ';
 look_and_say( $data, 40 );
 echo PHP_EOL . 'Part 2: ';
-//look_and_say( $data, 50 );
+look_and_say( $data, 50 );
+echo PHP_EOL. 'Total time to generate: ' . ( microtime( true ) - $starttime );
 echo PHP_EOL . PHP_EOL;
