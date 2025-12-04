a16f19c385a4c597f815cfb1ae761ffdd0aa0290
Brian Fischer <brian@linchpin.com>
2025-12-04 01:00:01 -0500

    AOC(2025): Day 4 - Complete

--- 2025/04.php
+++ 2025/04.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Day 04: Printing Department
+ * Part 1: 0.02908 Seconds (20m32s - 4393rd)
+ * Part 2: 0.85484 Seconds (34m44s - 4464th)
  */
 
 // The usual
@@ -8,80 +10,20 @@ $starttime = microtime(true);
 $data = file_get_contents('data/data-04.txt');
 $data = file_get_contents('data/data-04-sample.txt');
 
-$rows = explode("\n", $data);
+$dataset = explode("\n", $data);
 
-$dataset = $rows;
-include_once( '../functions.php' );
-
-// Part One
-function part_one($dataset) {
+// Part One and Two
+function part_one_and_two($dataset, $keep_going = false) {
 
 	$map = [];
 	$check = [];
 	$y_bounds = sizeof( $dataset ) - 1;
 	$x_bounds = strlen( $dataset[0] ) - 1;
-	$allowed = 0;
+	
+	$accessible = 0;
 
-	foreach ( $dataset as $y => $row ) {
-		foreach ( str_split( $row ) as $x => $char ) {
-			$map[$y][$x] = $char;
-			if ( $char === '@' ) {
-				$check[] = "$y,$x";
-			}
-		}
-	}
-
-	foreach( $check as $c ) {
-		// echo "Checking $c: ";
-		[$y, $x] = explode( ',', $c );
-
-		$roll_total = 0;
-
-		$dirs = [
-			[-1, -1],
-			[-1, 1],
-			[1, -1],
-			[1,1],
-			[-1, 0],
-			[1,0],
-			[0,1],
-			[0,-1]
-		];
-
-		foreach( $dirs as $dir ) {
-			[$dy, $dx] = $dir;
-			if ( 
-				( $y + $dy ) >= 0 &&
-				( $y + $dy ) <= $y_bounds &&
-				( $x + $dx ) >= 0 &&
-				( $x + $dx ) <= $y_bounds
-			) {
-				if ( ( $map[ $y + $dy ][ $x + $dx ] === '@' ) ) {
-					$roll_total++;
-				}
-			}
-		}
-
-		// echo "y: $roll_total;";
-		// echo PHP_EOL;
-
-		if ( $roll_total < 4 ) {
-			$allowed++;
-		}
-	}
-
-	echo $allowed;
-}
-
-// Part Two
-function part_two($dataset) {
-	$map = [];
-	$check = [];
-	$y_bounds = sizeof( $dataset ) - 1;
-	$x_bounds = strlen( $dataset[0] ) - 1;
-	$allowed = 0;
-
-	$removed = 1;
+	// Set above 0 to run
+	$removed = 999;
 
 	foreach ( $dataset as $y => $row ) {
 		foreach ( str_split( $row ) as $x => $char ) {
@@ -92,19 +34,18 @@ function part_two($dataset) {
 		}
 	}
 
+	// While we're removing at least one roll...
 	while ( $removed > 0 ) {
 
-		// echo PHP_EOL . "New Run " . sizeof( $check ) . ": ";
-
-
-		$removed_this_time = 0;
+		// Reset for subsequent runs
+		$removed = 0;
 		$cleared = [];
 
+		// Check all coordinates
 		foreach( $check as $c ) {
-			// echo "Checking $c: ";
 			[$y, $x] = explode( ',', $c );
 
-			$roll_total = 0;
+			$adj_rolls = 0;
 
 			$dirs = [
 				[-1, -1],
@@ -125,40 +66,44 @@ function part_two($dataset) {
 					( $x + $dx ) >= 0 &&
 					( $x + $dx ) <= $y_bounds
 				) {
+					// If there's another roll nearby, increase
 					if ( ( $map[ $y + $dy ][ $x + $dx ] === '@' ) ) {
-						$roll_total++;
+						$adj_rolls++;
 					}
 				}
 			}
 
-			// echo "y: $roll_total;";
-			// echo PHP_EOL;
-
-			if ( $roll_total < 4 ) {
+			// If surrounded by 4 or less rolls, we can grab it
+			// Add to the list to be removed after this run, increase the total
+			if ( $adj_rolls < 4 ) {
 				$cleared[] = [$y, $x];
-				$removed_this_time++;
-				$allowed++;
+				$removed++;
+				$accessible++;
 			}
 		}
 
-		// echo PHP_EOL . $removed_this_time . PHP_EOL;
+		// Keep running, or stop at one run
+		if ( $keep_going ) {
 
-		$removed = $removed_this_time;
+			// Remove any rolls we've grabbed from the map and the check
+			foreach( $cleared as $c ) {
+				[$y, $x] = $c;
+				$map[ $y ][ $x ] = '.';
+				unset( $check[ $y . ',' . $x ] );
+			}
 
-		foreach( $cleared as $c ) {
-			[$y, $x] = $c;
-			$map[ $y ][ $x ] = '.';
-			unset( $check[ $y . ',' . $x ] );
+		} else {
+			$removed = 0;
 		}
 	}
 
-	echo $allowed;
+	echo $accessible;
 }
 
 echo PHP_EOL . 'Day 04: Printing Department' . PHP_EOL . 'Part 1: ';
-part_one($dataset);
+part_one_and_two( $dataset );
 echo PHP_EOL . 'Part 2: ';
-part_two($dataset);
+part_one_and_two( $dataset, true) ;
 echo PHP_EOL;
 echo 'Total time to generate: ' . ( microtime( true ) - $starttime );
 echo PHP_EOL;
